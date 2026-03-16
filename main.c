a0429ba963a3b1cb85f1137ea54c1f5956b4c059
felixjaschul <felix@jaschul.de>
2026-03-16 12:28:24 +0100

    drawing

--- main.c
+++ main.c
@@ -18,21 +18,31 @@ struct {
 #define A(x) do { if (!(x)) { printf("Assertion failed: %s\n", #x); exit(1); } } while(0)
 #define L(x) do { printf("%s\n", x); } while(0)
 
-int main(void)
+#define WIDTH 1270
+#define HEIGHT 800
+
+void draw(const int screen, const unsigned int bs)
+{
+    /* drawing */ {
+        /* background */ for (int i = 0; i < WIDTH*HEIGHT; i++) {
+            if (((i % WIDTH)/bs + (i / WIDTH)/bs) % 2 == 0) s.pixels[i] = 0x676767;
+            else s.pixels[i] = 0x767676;
+        }
+    } XPutImage(s.display, s.window, DefaultGC(s.display, screen), s.ximage, 0,0,0,0, WIDTH, HEIGHT);
+}
+
+int main()
 {
     s.display = XOpenDisplay(NULL);
     A(s.display);
 
     auto screen = DefaultScreen(s.display);
 
-#define width 1270
-#define height 800
-
-    s.pixels = malloc(sizeof(unsigned int)*width*height);
+    s.pixels = malloc(sizeof(unsigned int)*WIDTH*HEIGHT);
     A(s.pixels);
 
     s.window = XCreateSimpleWindow(
-        s.display, RootWindow(s.display, screen), None, None, width, height, None, None, BlackPixel(s.display, screen));
+        s.display, RootWindow(s.display, screen), None, None, WIDTH, HEIGHT, None, None, BlackPixel(s.display, screen));
 
     XStoreName(s.display, s.window, "xeleven _f");
 
@@ -43,9 +53,10 @@ int main(void)
     XMapWindow(s.display, s.window);
 
     s.ximage = XCreateImage(
-        s.display, DefaultVisual(s.display, screen), DefaultDepth(s.display, screen), ZPixmap, 0, (char*)s.pixels, width, height, 32, 0);
+        s.display, DefaultVisual(s.display, screen), DefaultDepth(s.display, screen), ZPixmap, 0, (char*)s.pixels, WIDTH, HEIGHT, 32, 0);
 
     s.running = 1;
+    unsigned int bs = 50;
     while (s.running)
     {
         XNextEvent(s.display, &s.event);
@@ -53,29 +64,22 @@ int main(void)
         switch (s.event.type)
         {
             case Expose:
-                /* drawing */ {
-                    const int bs = 50;
-                    for (int i = 0; i < width*height; i++) {
-                        if (((i % width)/bs + (i / width)/bs) % 2 == 0) s.pixels[i] = 0x670067;
-                        else s.pixels[i] = 0x67;
-                    }
-                } XPutImage(s.display, s.window, DefaultGC(s.display, screen), s.ximage, 0,0,0,0, width, height);
-                break;
+                draw(screen, bs);
             case KeyPress:
                 const KeySym key = XLookupKeysym(&s.event.xkey, 0);
                 if (key == XK_Escape) s.running = 0;
-                if (key == XK_Return) L("hi");
-                break;
+                if (key == XK_plus) if(bs <= 200)bs+=1;
+                if (key == XK_minus) if(bs >= 10)bs-=1;
+                draw(screen, bs);
             case ClientMessage:
                 if ((Atom)s.event.xclient.data.l[0] == wm_delete) s.running = 0;
                 break;
-            default:
-                break;
+            default: break;
         }
     }
 
-#undef width
-#undef height
+#undef WIDTH
+#undef HEIGHT
     XDestroyWindow(s.display, s.window);
     XCloseDisplay(s.display);
     return 0;
